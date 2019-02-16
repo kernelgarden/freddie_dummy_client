347df6671b27ad1c376180779b96af99c7e998c2
Kernelgarden <kernelgarden@gmail.com>
2019-02-16 23:29:03 +0900

    change sending process

--- lib/freddie_client/echo.ex
+++ lib/freddie_client/echo.ex
@@ -3,8 +3,13 @@ defmodule FreddieClient.Echo do
   alias FreddieClient.Scheme
 
   def send_echo(socket, msg) do
+    req = FreddieClient.Echo.make_msg(msg)
+    FreddieClient.Transport.send(socket, req)
+  end
+
+  def make_msg(msg) do
     echo = Scheme.Echo.new(msg: msg)
     {:ok, packet} = Freddie.Scheme.Common.new_message(0, Scheme.Echo.encode(echo))
-    FreddieClient.Transport.send(socket, packet)
+    packet
   end
 end
